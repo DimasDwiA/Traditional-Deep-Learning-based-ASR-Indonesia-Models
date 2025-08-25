7348d4090560af1e2f2291ff7c06730965361951
Dimas Dwi Armaisya <ddadims04@gmail.com>
2025-08-25 08:15:11 +0700

    Update code audio_utils.py

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,7 +1,20 @@
 import soundfile as sf
 import numpy as np
+import librosa
+import random
 import tensorflow as tf
 from keras.saving import register_keras_serializable
+from mltu.tensorflow.losses import CTCloss as OriginalCTCLoss
+
+# Class to adjust CTCLoss
+class CTCLossFixed(tf.keras.losses.Loss):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.base_loss = OriginalCTCLoss(*args, **kwargs)
+
+    def call(self, y_true, y_pred):
+        loss = self.base_loss(y_true, y_pred)
+        return tf.reduce_mean(loss)
 
 # Function to save audio file correctly
 def save_audio_file(audio_array, sample_rate, file_path):
